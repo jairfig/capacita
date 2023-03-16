6a72aacb120ff53d5f47e2ad84041c5f6e3a87b6
Jair Figueiredo <jairfigueiredo@gmail.com>
2023-03-15 23:18:38 -0500

    Boostrap5 configurado.

---
+++ painel/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def painel(request):
+    context = {
+        'local': "painel",
+    }
+    return render(request, 'painel/painel.html', context)
+
